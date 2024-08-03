92c87e493b72aa8aa67aa81c060da2fc76b2460c
Angel Lopez <a.o.lopez.cruz@icloud.com>
2024-08-02 19:13:35 -0600

    Initial Commit

---
+++ RockGame/RockGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockGameApp.swift
+//  RockGame
+//
+//  Created by Diplomado on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RockGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
